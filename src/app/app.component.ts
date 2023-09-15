21b2534c80bd92d80164d2f08a383cdaed490451
LAPTOP-FAPJ05T7\david <davidecaziuc3@gmail.com>
2023-09-15 11:47:00 +0200

    inizio ifp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SediService } from 'src/services/sedi.service'; // Assicurati di utilizzare il percorso corretto verso il tuo servizio
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ifp';
+export class AppComponent implements OnInit{
+  title = 'IFP';
+  sidebarVisible = true;
+  categories: string[] = ["Sedi", "Docenti", "Corsi"];
+  selectedCategories: any[] = [];
+  listaSedi: any[] = [];
+
+  constructor(private sediService: SediService) {}
+
+  ngOnInit(): void {
+    this.sediService.getListaSedi().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.listaSedi = data;
+      },
+      (error) => {
+        console.error('Errore nella chiamata API:', error);
+      }
+    );
+  }
+  
+
+  openSidebar() {
+    this.sidebarVisible = !this.sidebarVisible  ;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { SplitterModule } from 'primeng/splitter';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SidebarModule,
+    SplitterModule,
+    CheckboxModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+ 
